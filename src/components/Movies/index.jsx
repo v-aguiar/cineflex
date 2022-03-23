f65c85c9acce7b65b249b8d4fcbc54d1d5a07344
v-aguiar <v.aguiar.dev@gmail.com>
2022-03-23 01:31:27 -0300

    Add initial component static infos to set layout to the first component -> movies

---
+++ src/components/Movies/index.jsx
@@ -0,0 +1,56 @@
+﻿import {Link} from "react-router-dom"
+
+import movie1 from "../../assets/img/movie1.png"
+import movie2 from "../../assets/img/movie2.png"
+
+import "./style.css"
+
+export default function Movies() {
+  return (
+    <section className="Movies">
+      <h2 className="description">Selecione o filme</h2>
+      <div className="container">
+        <Link to="/sessoes/:movieId" >
+          <div className="imgWrapper">
+            <img src={movie1} alt="Movie cover picture" />
+          </div>
+        </Link>
+        <Link to="/sessoes/:movieId" >
+          <div className="imgWrapper">
+            <img src={movie2} alt="Movie cover picture" />
+          </div>
+        </Link>
+        <Link to="/sessoes/:movieId" >
+          <div className="imgWrapper">
+            <img src={movie1} alt="Movie cover picture" />
+          </div>
+        </Link>
+        <Link to="/sessoes/:movieId" >
+          <div className="imgWrapper">
+            <img src={movie2} alt="Movie cover picture" />
+          </div>
+        </Link>
+        <Link to="/sessoes/:movieId" >
+          <div className="imgWrapper">
+            <img src={movie1} alt="Movie cover picture" />
+          </div>
+        </Link>
+        <Link to="/sessoes/:movieId" >
+          <div className="imgWrapper">
+            <img src={movie2} alt="Movie cover picture" />
+          </div>
+        </Link>
+        <Link to="/sessoes/:movieId" >
+          <div className="imgWrapper">
+            <img src={movie1} alt="Movie cover picture" />
+          </div>
+        </Link>
+        <Link to="/sessoes/:movieId" >
+          <div className="imgWrapper">
+            <img src={movie2} alt="Movie cover picture" />
+          </div>
+        </Link>
+      </div>
+    </section>
+  )
+}
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -4,13 +4,14 @@ import Header from "./Header"
 
 import "../assets/styles/reset.css"
 import "../assets/styles/style.css"
+import Movies from "./Movies"
 
 export default function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-
+        <Route path="/" element={<Movies />} />
       </Routes>
     </BrowserRouter>
   )
